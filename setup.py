7532d26942fceebcce3343bc432b0d87dd44bba7
MitraMitraMitra <62009716+MitraMitraMitra@users.noreply.github.com>
2022-02-02 00:53:10 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'letters-plot',
-  packages = ['letters-plot'],
+  name = 'lettersPoints',
+  packages = ['lettersPoints'],
   version = '0.1',
   license = 'MIT',
   description = 'This library can be used to generate points that, when plotted, form letters. The height and spacing can be set.',
   author = 'Jean-Paul Mitra',
   author_email = 'jeanmitra77@gmail.com',
   url = 'https://github.com/MitraMitraMitra/letters-plot',
-  download_url = 'https://github.com/MitraMitraMitra/letters-plot/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/MitraMitraMitra/letters-plot/archive/refs/tags/v_03.tar.gz',    # I explain this later on
   keywords = ['plotting', 'letters'],
   install_requires = [
           'math',
